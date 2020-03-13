45ba51ccf4bf9571dcfc1cb26caa24256edf689e
Vladimir <vladsfx@gmail.com>
2020-03-13 11:24:43 +0300

    adding teacher data

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let money = prompt('Ваш бюджет на месяц', ''),
+let money = +prompt('Ваш бюджет на месяц', ''),
     time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
 let appData = {
@@ -16,7 +16,25 @@ for (let i = 0; i < 2; i++) {
     let article = prompt('Введите обязательную статью расходов в этом месяце', ''),
         amount = prompt('Во сколько обойдётся?', '');
 
-    appData.expenses[article] = amount;
+    if ((typeof (article)) === 'string' && (typeof (article)) != null && (typeof (amount)) != null &&
+        article != '' && amount != '' && article.length < 50) {
+        console.log('done');
+        appData.expenses[article] = amount;
+    } else {
+
+    }
 }
 
-alert('Бюджет на 1 день составляет ' + appData.budget / 30 + ' рублей');
+appData.moneyPerDay = appData.budget / 30;
+
+alert('Ежедневный бюджет: ' + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    console.log('Минимальный уровень достатка');
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log('Средний уровень достатка');
+} else if (appData.moneyPerDay > 2000) {
+    console.log('Высокий уровень достатка');
+} else {
+    console.log('Произошла ошибка');
+}
